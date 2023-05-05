64e65680fa29b655eee0077ec301053f19e665c4
Bryan Neveu <bryan@pando-studio.com>
2023-05-05 14:49:46 +0200

    add : admin role

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -7,19 +7,18 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 
-
 /**
- * @Security("is_granted('ROLE_USER')")
+ * @Security("is_granted('ROLE_ADMIN')")
  */
-class GameController extends AbstractController
+class AdminController extends AbstractController
 {
-    #[Route('/game', name: 'app_game')]
+    #[Route('/admin', name: 'app_admin')]
     public function index(): Response
     {
-        $this->denyAccessUnlessGranted('ROLE_USER');
+        $this->denyAccessUnlessGranted('ROLE_ADMIN');
 
-        return $this->render('game/index.html.twig', [
-            'controller_name' => 'GameController',
+        return $this->render('admin/index.html.twig', [
+            'controller_name' => 'AdminController',
         ]);
     }
 }
