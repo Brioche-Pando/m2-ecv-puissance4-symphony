0af2832fce1431ea2a4e34ecced19ebf74c3e6a7
Bryan Neveu <bryan@pando-studio.com>
2023-05-12 11:22:58 +0200

    add : admin controller can see preview of each games played

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -6,6 +6,7 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\GameRepository;
 
 /**
  * @Security("is_granted('ROLE_ADMIN')")
@@ -13,12 +14,19 @@ use Symfony\Component\Routing\Annotation\Route;
 class AdminController extends AbstractController
 {
     #[Route('/admin', name: 'app_admin')]
-    public function index(): Response
+    public function index(GameRepository $gameRepository): Response
     {
         $this->denyAccessUnlessGranted('ROLE_ADMIN');
 
+        $games = $gameRepository->findAll();
+
+        foreach ($games as $game) {
+            $board = $game->getBoard();
+            dump($board);
+        }
+
         return $this->render('admin/index.html.twig', [
-            'controller_name' => 'AdminController',
+            'games' => $games,
         ]);
     }
 }
