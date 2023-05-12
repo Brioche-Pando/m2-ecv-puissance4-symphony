9d1aa63bb67e2d1575a26108f2cdff3902f37434
Bryan Neveu <bryan@pando-studio.com>
2023-05-12 16:12:01 +0200

    add : UserBadge detect if user is blocked

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -4,9 +4,11 @@ namespace App\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Repository\GameRepository;
+use App\Repository\UserRepository;
 
 /**
  * @Security("is_granted('ROLE_ADMIN')")
@@ -14,19 +16,33 @@ use App\Repository\GameRepository;
 class AdminController extends AbstractController
 {
     #[Route('/admin', name: 'app_admin')]
-    public function index(GameRepository $gameRepository): Response
+    public function index(GameRepository $gameRepository, UserRepository $userRepository): Response
     {
         $this->denyAccessUnlessGranted('ROLE_ADMIN');
 
         $games = $gameRepository->findAll();
-
-        foreach ($games as $game) {
-            $board = $game->getBoard();
-            dump($board);
-        }
+        $users = $userRepository->findAll();
 
         return $this->render('admin/index.html.twig', [
+            'users' => $users,
             'games' => $games,
         ]);
+    }
+
+    // Blocage/Déblocage d'un utilisateur
+    #[Route('/admin/users/{userId}/block', name: 'admin_block_user')]
+    public function blockUser(Request $request, UserRepository $userRepository, int $userId): Response
+    {
+        $user = $userRepository->find($userId);
+
+        if (!$user) {
+            throw $this->createNotFoundException('Utilisateur non trouvé.');
+        }
+
+        // Inverser l'état de blocage
+        $user->setBlocked(!$user->isBlocked());
+        $userRepository->save($user, true);
+
+        return $this->redirectToRoute('app_admin');
     }
 }
--- src/Security/AppAuthenticator.php
+++ src/Security/AppAuthenticator.php
@@ -2,11 +2,13 @@
 
 namespace App\Security;
 
+use App\Repository\UserRepository;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\Exception\CustomUserMessageAuthenticationException;
 use Symfony\Component\Security\Core\Security;
 use Symfony\Component\Security\Http\Authenticator\AbstractLoginFormAuthenticator;
 use Symfony\Component\Security\Http\Authenticator\Passport\Badge\CsrfTokenBadge;
@@ -15,13 +17,14 @@ use Symfony\Component\Security\Http\Authenticator\Passport\Credentials\PasswordC
 use Symfony\Component\Security\Http\Authenticator\Passport\Passport;
 use Symfony\Component\Security\Http\Util\TargetPathTrait;
 
+
 class AppAuthenticator extends AbstractLoginFormAuthenticator
 {
     use TargetPathTrait;
 
     public const LOGIN_ROUTE = 'app_login';
 
-    public function __construct(private UrlGeneratorInterface $urlGenerator)
+    public function __construct(private UrlGeneratorInterface $urlGenerator, private UserRepository $userRepository)
     {
     }
 
@@ -32,7 +35,13 @@ class AppAuthenticator extends AbstractLoginFormAuthenticator
         $request->getSession()->set(Security::LAST_USERNAME, $email);
 
         return new Passport(
-            new UserBadge($email),
+            new UserBadge($email, function ($userIdentifier) {
+                $user = $this->userRepository->findOneBy(['email' => $userIdentifier]);
+                if ($user->isBlocked()) {
+                    throw new CustomUserMessageAuthenticationException('Votre compte est bloqué. Veuillez contacter un administrateur.');
+                }
+                return $user;
+            }),
             new PasswordCredentials($request->request->get('password', '')),
             [
                 new CsrfTokenBadge('authenticate', $request->request->get('_csrf_token')),
