6d40cefb3b6206d23cb7d035a60b89704cdb366d
Bryan Neveu <bryan@pando-studio.com>
2023-06-02 17:29:17 +0200

    add : json xml serializer

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -9,6 +9,10 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Repository\GameRepository;
 use App\Repository\UserRepository;
+use Symfony\Component\Serializer\Encoder\JsonEncoder;
+use Symfony\Component\Serializer\Encoder\XmlEncoder;
+use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
+use Symfony\Component\Serializer\Serializer;
 
 /**
  * @Security("is_granted('ROLE_ADMIN')")
@@ -44,5 +48,19 @@ class AdminController extends AbstractController
         $userRepository->save($user, true);
 
         return $this->redirectToRoute('app_admin');
+    }
+
+    #[Route('/admin/game/{gameId}.{extension}', name: 'admin_serialized_game')]
+    public function serialized(GameRepository $gameRepository, int $gameId, string $extension): Response
+    {
+        $game = $gameRepository->find($gameId);
+        $board = $game->getBoard();
+
+        $encoders = [new XmlEncoder(), new JsonEncoder()];
+        $normalizers = [new ObjectNormalizer()];
+
+        $serializer = new Serializer($normalizers, $encoders);
+
+        return new Response($serializer->serialize($board, $extension), 200, ['content-type' => 'text/' . $extension]);
     }
 }
--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -26,7 +26,7 @@ class RegistrationController extends AbstractController
     {
         $this->emailVerifier = $emailVerifier;
     }
-    // #[Route('/register', name: 'app_register')]
+    
     #[Route('/register/{_locale}', name: 'app_register')]
     public function register(Request $request, UserPasswordHasherInterface $userPasswordHasher, UserAuthenticatorInterface $userAuthenticator, AppAuthenticator $authenticator, EntityManagerInterface $entityManager): Response
     {
